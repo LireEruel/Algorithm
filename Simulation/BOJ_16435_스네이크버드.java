1c7e7c6199241d898845638ac740c06da15279c7
LireEruel <jsh001505@gmail.com>
2023-08-11 15:55:52 +0900

    백준 16435 스네이크 버드

--- Simulation/BOJ_16435_스네이크버드.java
+++ Simulation/BOJ_16435_스네이크버드.java
@@ -1,4 +1,28 @@
 package Simulation;
 
+import java.io.*;
+import java.util.*;
+
 public class BOJ_16435_스네이크버드 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(new File("input.txt")));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        int height = Integer.parseInt(st.nextToken());
+
+        int[] foods = new int[size];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < size; i++) {
+            foods[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(foods);
+        for (int food: foods ) {
+            if(food <= height) {
+                height++;
+            }else {
+                break;
+            }
+        }
+        System.out.println(height);
+    }
 }
