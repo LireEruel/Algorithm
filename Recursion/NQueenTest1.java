fa5fc9070d0a56643ce45d01fa3f6e76c579bab9
LireEruel <jsh001505@gmail.com>
2023-08-16 11:30:59 +0900

    브론즈 푸는중

---
+++ Recursion/NQueenTest1.java
@@ -0,0 +1,43 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class NQueenTest1 {
+    static int N, col[], ans;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        col = new int[N+1];
+        ans = 0;
+        
+        setQueen(1);
+        System.out.println(a);
+    }
+    private static void setQueen(int row){
+
+        //가지치기
+        if(!isAvaillable(row)) return;
+        //가지조건
+        if(row > N) {
+            ans++;
+            return;
+        }
+
+        //유도파트
+        for(int c = 1; c <= N; c++){
+            col[row] = c;
+            setQueen(row + 1);
+        }
+        setQueen(row+1);
+    }
+
+    private static boolean isAvaillable(int row) {
+        for (int i = 0; i < row; i++) {
+            if (col[i] == col[row] || row -i == (int)Math.abs(col[row] - col[i])) {
+                 return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Simulation/BOJ_11720_숫자의_합.py
@@ -0,0 +1,6 @@
+T = int(input())
+sum = 0
+nums = input()
+for num in nums:
+    sum += int(num)
+print(sum)
