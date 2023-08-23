d0beeb8c76483ff78456e58d2477a5977cc8dbc8
LireEruel <jsh001505@gmail.com>
2023-08-23 15:36:34 +0900

    백준 1197 최소 스패닝 트리

---
+++ Kruskal/BOJ_1197_최소_스패닝_트리.java
@@ -0,0 +1,75 @@
+package Kruskal;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1197_최소_스패닝_트리 {
+
+    static class Edge implements Comparable<Edge> {
+        int from, to, weight;
+
+        public Edge(int from, int to, int weight){
+            super();
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o){
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+
+    static Edge[] edgeList;
+    static int[] parents;
+    static int V,E;
+    static int find(int a){
+        if(parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
+    }
+
+    private static void make () {
+        parents = new int[V];
+        for(int i = 0; i < V; i++){
+            parents[i] = i;
+        }
+    }
+    static boolean union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if(aRoot == bRoot) return false;
+        parents[bRoot] = aRoot;
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        make();
+        edgeList = new Edge[E];
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            Edge edge = new Edge(from, to, weight);
+            edgeList[i]= edge;
+        }
+        Arrays.sort(edgeList);
+        int result = 0;
+        int count = 0;
+        for(Edge edge: edgeList){
+            if(union(edge.from, edge.to)){
+                result += edge.weight;
+                if(++count == V-1) break;
+            }
+        }
+        System.out.println(result);
+    }
+}
