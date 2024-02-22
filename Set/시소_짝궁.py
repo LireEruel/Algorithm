7d96139a579c9fbc25a081b06b39be8fd760df34
LireEruel <jsh001505@gmail.com>
2024-02-22 21:55:11 +0900

    시소 짝궁 통과

--- Set/시소_짝궁.py
+++ Set/시소_짝궁.py
@@ -1,9 +1,10 @@
 #########################################
 #
 # https://school.programmers.co.kr/learn/courses/30/lessons/152996?language=python3
-# 통과 못함 ㅠㅠ 서러움
+#
 #
 #########################################
+
 
 
 def solution(weights):
@@ -13,15 +14,17 @@ def solution(weights):
         count = weights.count(w) 
         if(count > 1):
             answer += count*(count-1)/2
-
-    weights = list(set(weights)) # 중복 제거
-    weights.sort()
-    for i in range(len(weights)-1): # 비교할 왼쪽
-        for d in range(i+1, len(weights)): # 오른쪽
-            s = weights[i] #작은거
-            l = weights[d] #큰거
+    wSet = list(set(weights)) # 중복 제거
+    wSet.sort()
+    for i in range(len(wSet)-1): # 비교할 왼쪽
+        for d in range(i+1, len(wSet)): # 오른쪽
+            s = wSet[i] #작은거
+            l = wSet[d] #큰거
             if(4*s == 3*l or 4*s == 2*l or 3*s == 2*l): #경우의 수 3가지 ( 44 33 22 는 같지 않으면 없음)
-                answer += 1 
+                print(s, l)
+                #print(weights.count(s), weights.count(l))
+                answer += weights.count(s) * weights.count(l)
                 
     return answer
 
+solution([100,180,360,100,270])
